f972477fea7203734e57526d3b2269424efc7a32
Ajinkya Temgire <ajinkya.temgire0835@gmail.com>
2026-02-02 08:28:52 +0530

    added the pydantic object and state for the graph

--- basic_ai.py
+++ basic_ai.py
@@ -1,1 +1,25 @@
-import pydantic
+from pydantic import BaseModel , Field
+from typing import List , Annotated , TypedDict
+import operator
+
+
+class Task(BaseModel):
+    id : int
+    title : str
+    brief: str = Field(... , description = "What to cover")
+
+class Plan(BaseModel):
+    blog_title :  str
+    task : List[Task]
+
+
+class State(TypedDict):
+    topic :  str
+    plan : Plan
+    # for reducer agent
+    sections :  Annotated[List[str] , operator.add]
+    final : str
+
+
+# Graph
+
